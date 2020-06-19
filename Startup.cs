27b3270de622d51ab91e436fe1078ae034a34299
prasanna.mormagondi@nttdata.com <prasanna.mormagondi@nttdata.com>
2020-06-19 14:53:55 +0530

    Intial commit

---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            _configuration = configuration;
+            _environment = env;
+        }
+        public IConfiguration _configuration { get; }
+        public IHostingEnvironment _environment { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                string _message = $"Hello World! - {_configuration["EnviromentTitle"]} - {_configuration["AppServiceTitle"]} - Environment: {_environment.EnvironmentName}";
+                await context.Response.WriteAsync(_message);
+            });
+        }
+    }
+}
